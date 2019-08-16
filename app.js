687f88af9e9b5045dc5ba7bbd53f58e8cb6c5404
Charlie Ylijoki <ylijokic@oregonstate.edu>
2019-08-15 22:46:12 -0700

    Setup Connection to Mapbox API

--- app.js
+++ app.js
@@ -1,31 +1,58 @@
-const mymap = L.map('mapid').setView([45.52, -122.67], 11);
+mapboxgl.accessToken =
+  'pk.eyJ1IjoieWxpam9raWMiLCJhIjoiY2p6YXMzOWYyMDA0bTNocnBkcWE5bThvbSJ9.tIcLrrsbnckIrFS_4D8Sug';
 
-L.tileLayer(
-  'https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}',
-  {
-    attribution:
-      'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken:
-      'pk.eyJ1IjoieWxpam9raWMiLCJhIjoiY2p6YXMzOWYyMDA0bTNocnBkcWE5bThvbSJ9.tIcLrrsbnckIrFS_4D8Sug'
+const map = new mapboxgl.Map({
+  container: 'map', // Container ID
+  style: 'mapbox://styles/mapbox/dark-v10', // Map style to use
+  center: [-122.675, 45.5051], // Starting position [lng, lat]
+  zoom: 12 // Starting zoom level
+});
+
+const marker = new mapboxgl.Marker() // initialize a new marker
+  .setLngLat([-122.675, 45.5051]) // Marker [lng, lat] coordinates
+  .addTo(map); // Add the marker to the map
+
+const geocoder = new MapboxGeocoder({
+  // Initialize the geocoder
+  accessToken: mapboxgl.accessToken, // Set the access token
+  mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: false, // Do not use the default marker style
+  placeholder: 'Search places in Portland',
+  bbox: [-122.9, 45.3, -122.3, 45.6], // Boundary for search
+  proximity: {
+    longitude: -122.675,
+    latitude: 45.5051
   }
-).addTo(mymap);
+});
 
-const circle = L.circle([45.52, -122.67], {
-  color: 'red',
-  fillColor: '#f03',
-  fillOpacity: 0.5,
-  radius: 3000
-}).addTo(mymap);
+// Add the geocoder to the map
+map.addControl(geocoder);
 
-const popup = L.popup();
+// After the map style has loaded on the page,
+// add a source layer and default styling for a single point
+map.on('load', function() {
+  map.addSource('single-point', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
 
-function onMapClick(e) {
-  popup
-    .setLatLng(e.latlng)
-    .setContent('You clicked the map at ' + e.latlng.toString())
-    .openOn(mymap);
-}
+  map.addLayer({
+    id: 'point',
+    source: 'single-point',
+    type: 'circle',
+    paint: {
+      'circle-radius': 10,
+      'circle-color': '#448ee4'
+    }
+  });
 
-mymap.on('click', onMapClick);
+  // Listen for the `result` event from the Geocoder
+  // `result` event is triggered when a user makes a selection
+  //  Add a marker at the result's coordinates
+  geocoder.on('result', function(e) {
+    map.getSource('single-point').setData(e.result.geometry);
+  });
+});
